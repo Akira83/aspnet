e9f03fbb28fe4ea71c129b623b6f33cf6def57a8
Serikawa <mserikawa@obsglobal.com>
2020-09-17 09:28:38 -0500

    End of section 4

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -9,24 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   registerMode: any;
-  values: any;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.getValues();
-  }
+  ngOnInit(): void { }
 
   registerToggle(): void {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.error(error);
-    });
+  cancelRegisterMode(registerMode: boolean): void {
+    this.registerMode = registerMode;
   }
-
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,24 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   register(): void {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successsful');
+    }, error => {
+      console.error(error);
+    });
   }
 
   cancel(): void {
+    this.cancelRegister.emit(false);
     console.log('cancelled');
   }
 
